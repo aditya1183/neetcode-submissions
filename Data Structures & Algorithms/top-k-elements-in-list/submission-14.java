16d485a88d4a9f4c28eb9d57697d9abd280f8db4
aditya <122526560+aditya1183@users.noreply.github.com>
2026-09-13 13:21:59 +0530

    Add: top-k-elements-in-list - submission-14

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-14.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // first we build one fre map that contains   the  number and freq
+
+        HashMap<Integer , Integer> freqMap= new HashMap<>();
+        for(int num : nums){
+            freqMap.put (num , freqMap.getOrDefault(num , 0)+1);
+        }
+
+        // build  min heap
+
+    PriorityQueue<int[] > heap =new PriorityQueue<>( (a, b) -> Integer.compare(a[0], b[0]));
+        for(Map.Entry<Integer , Integer> entry :  freqMap.entrySet()){
+            heap.offer(new int[]{entry.getValue() , entry.getKey()});
+            if(heap.size() > k){
+                heap.poll();
+            }
+
+        }
+        int[] ans =new int[k]; // result array 
+
+        for(int i=0 ; i < k  ; i++){
+             ans[i] = heap.poll()[1];
+        }
+        return ans;
+        
+
+
+
+        
+    }
+}
